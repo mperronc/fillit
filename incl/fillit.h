b911e89135bf9d7d29d95c4f1c13f302dea80244
Diana BOUDY <dboudy@e3r1p14.42.fr>
2015-12-15 11:05:48 +0100

    update fillit.h and remove function with not Unixcase

--- incl/fillit.h
+++ incl/fillit.h
@@ -6,7 +6,7 @@
 /*   By: mperronc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/11 16:08:24 by mperronc          #+#    #+#             */
-/*   Updated: 2015/12/15 06:30:21 by mperronc         ###   ########.fr       */
+/*   Updated: 2015/12/15 10:40:53 by dboudy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,22 +27,22 @@ typedef struct		s_tetri
 	int				size_y;
 	char			pattern[4][4];
 	int				pos;
-	struct s_tetri *next;
+	struct s_tetri	*next;
 }					t_tetri;
 
-int		get_next_line(int const fd, char **line);
-int		is_pattern_valid(char *pattern);
-char	*twoD_to_oneD(char **pattern);
-int		get_offset(char *pattern);
-int		is_pattern_valid_dotsharp(char *pattern);
-int		is_pattern_valid_O(char *pattern, int offset);
-int		is_pattern_valid_I(char *pattern, int offset);
-int		is_pattern_valid_L(char *pattern, int offset);
-int		is_pattern_valid_J(char *pattern, int offset);
-int		is_pattern_valid_T(char *pattern, int offset);
-int		is_pattern_valid_S(char *pattern, int offset);
-int		is_pattern_valid_Z(char *pattern, int offset);
-void	push_tetrimino_back(t_tetri **first, char *pattern);
-t_tetri		*new_tetrimino(char *pattern, int pos);
+int					get_next_line(int const fd, char **line);
+int					is_pattern_valid(char *pattern);
+char				*twod_to_oned(char **pattern);
+int					get_offset(char *pattern);
+int					is_pattern_valid_dotsharp(char *pattern);
+int					is_pattern_valid_o(char *pattern, int offset);
+int					is_pattern_valid_i(char *pattern, int offset);
+int					is_pattern_valid_l(char *pattern, int offset);
+int					is_pattern_valid_j(char *pattern, int offset);
+int					is_pattern_valid_t(char *pattern, int offset);
+int					is_pattern_valid_s(char *pattern, int offset);
+int					is_pattern_valid_z(char *pattern, int offset);
+void				push_tetrimino_back(t_tetri **first, char *pattern);
+t_tetri				*new_tetrimino(char *pattern, int pos);
 
 #endif
